edcdf8333a4a9fa4090b7bd50cdccf0d6446401b
rumaikram <49103918+rumaikram@users.noreply.github.com>
2020-02-17 17:56:30 -0600

    Added Program narration

--- Python Project 1- Claiming_Prizes_Powerball.py
+++ Python Project 1- Claiming_Prizes_Powerball.py
@@ -1,3 +1,12 @@
+/*/In this project, the grand prize (in dollars) and the winning numbers are given. The program reads multiple tickets 
+   (6 selected numbers in each ticket) from the user.  For each ticket, the program calculates its prize won, and outputs 
+   the prize (in dollars) to the user.
+   Game rules:
+   In each game, players select 5 numbers from a set of 69 white balls (numbered 1 to 69) and one number from 26 red balls 
+   (numbered 1 to 26).  The red ball number can be the same as one of the white balls, but a white ball can only be selected once.  
+   The order of selection does not matter.  The numbers in each ticket is not guaranteed to be sorted in any way.
+
+----------------------------------------------------------------------------------------------------------------------------------------
 # 2 12 16 29 54 6
 GRAND_PRIZE = "$242,000,000"
 WHITE = [2, 12, 16, 29, 54]
@@ -42,8 +51,7 @@ while True:
             break
     else:
         print("Sorry, your response was not loud enough.")
-        continue
-# #           
+        continue          
      
 
 
